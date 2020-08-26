e7a51ffd933afc33fa7bc08d04c80d451b615a1d
gmlwn7245 <gmlwn7245@naver.com>
2020-08-26 23:33:11 +0900

    vuex1

---
+++ vuex-test/src/components/Child.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <hr>
+        Child counter : {{ childCounter }} <br>
+    </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+    computed:{
+     ...mapGetters({
+      childCounter : 'getCounter'
+      })
+    },
+    
+}
+</script>
---
+++ vuex-test/src/store.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex); // vue에 vuex등록
+
+export const store = new Vuex.Store({
+    state: {
+        counter: 0
+    },
+    getters: {
+        getCounter(state) {
+            return state.counter;
+        }
+    },
+    mutations: { 
+        addCounter(state) {
+            return state.counter++;
+        },
+        subCounter(state) {
+            return state.counter--;
+        }
+    },
+    actions: {
+        addCounter(context) {
+            return context.commit('addCounter');
+        }
+    }
+});
